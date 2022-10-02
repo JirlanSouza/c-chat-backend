7b84fc54a126b9d7d08c74e59f4c5a427787d6d9
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-02 12:14:44 -0400

    Created the AuthenticateUserUseCase tests

---
+++ tests/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,62 @@
+import { IRegisterUserDTO } from "@application/accounts/dtos/IRegisterUserDTO";
+import { AuthenticateUserUseCase } from "@application/accounts/useCases/AuthenticateUser";
+import { RegisterUserUseCase } from "@application/accounts/useCases/RegisterUser";
+import { UsersInMemoryRepository } from "@infra/database/repositories/inMemory/UsersInMemoryRepository";
+import { AppError } from "@shared/errors/AppError";
+
+let usersInMemoryRepository: UsersInMemoryRepository;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let registerUserUseCase: RegisterUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersInMemoryRepository = new UsersInMemoryRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersInMemoryRepository);
+    registerUserUseCase = new RegisterUserUseCase(usersInMemoryRepository);
+  });
+
+  it("Should be able to authenticate an user", async () => {
+    const userData: IRegisterUserDTO = {
+      name: "New user",
+      email: "newuser@cchat.com",
+      password: "newUser",
+    };
+
+    await registerUserUseCase.execute(userData);
+    process.env.SECRET = "secret";
+
+    const authenticationData = await authenticateUserUseCase.execute({
+      email: userData.email,
+      password: userData.password,
+    });
+
+    expect(authenticationData).toHaveProperty("token");
+  });
+
+  it("Should not be able to authenticate an nonexistent user", async () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "newUsser@cchat.com",
+        password: "newUser",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to authenticate with incorrect password", async () => {
+    expect(async () => {
+      const userData: IRegisterUserDTO = {
+        name: "New user",
+        email: "newuser@cchat.com",
+        password: "newUser",
+      };
+
+      await registerUserUseCase.execute(userData);
+      process.env.SECRET = "secret";
+
+      await authenticateUserUseCase.execute({
+        email: userData.email,
+        password: "incorrect password",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
