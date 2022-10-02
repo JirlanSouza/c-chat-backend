5742f0b59641681f3c1f13869f1bb7ae0433afbf
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-02 12:14:15 -0400

    Created the UsersInMemoryRepository implementation

---
+++ src/infra/database/repositories/inMemory/UsersInMemoryRepository.ts
@@ -0,0 +1,20 @@
+import { IUsersRepository } from "@application/accounts/repositories/IUsersRepository";
+import { User } from "@domain/entities/User";
+
+export class UsersInMemoryRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  async save(user: User): Promise<void> {
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find((user) => user.email === email);
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+}
