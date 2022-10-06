362034aeec2f0849e5a3a15d9fc87a5dcf22a23b
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-05 22:30:34 -0400

    Changed the SocketioEventEmmitterGatway

--- src/infra/webSocket/events/IEventGatway.ts
+++
@@ -1,5 +0,0 @@
-export interface IEventGatway {
-  userConnected(connection: any): Promise<void>;
-  userDisconnected(): Promise<void>;
-  onMessage(message: any): Promise<void>;
-}
--- src/infra/webSocket/events/eventGatway.ts
+++ src/infra/webSocket/events/eventGatway.ts
@@ -1,37 +1,56 @@
-import { ChatMessage } from "@domain/entities/ChatMessage";
 import { Server, Socket } from "socket.io";
+import { EventHandler } from "./handlers/types";
 
-import { IEventGatway } from "./IEventGatway";
+import { EventEmitterGatway } from "./EventEmitterGatway";
+import { Logger } from "@shared/logger";
 
 type ConnectedUsers = Map<string, string>;
 
-const io = new Server();
-
-io.use((socket, next) => {
-  socket.handshake.auth.token;
-});
-
-export class SocketIoEventGatway implements IEventGatway {
+export class SocketIoEventGatway implements EventEmitterGatway {
   private connectedusers: ConnectedUsers;
+  private subscribers: Map<string, EventHandler>;
+  private connectEventSubscribers: Map<string, EventHandler>;
+  private disconnectEventSubscribers: Map<string, EventHandler>;
+  private errorEventSubscribers: Map<string, EventHandler>;
 
   constructor(private socket: Server) {
     this.connectedusers = new Map();
+    this.subscribers = new Map();
+    this.connectEventSubscribers = new Map();
+    this.disconnectEventSubscribers = new Map();
+    this.errorEventSubscribers = new Map();
+  }
 
+  async onEvents() {
     this.socket.on("connection", this.userConnected.bind(this));
-    this.socket.on("disconnect", this.userDisconnected.bind(this));
+    this.socket.on("disconnecting", this.userDisconnected.bind(this));
+  }
+
+  subscribeEvent(eventName: string, handler: EventHandler) {
+    this.subscribers.set(eventName, handler);
   }
 
   async userConnected(connection: Socket): Promise<void> {
-    connection.on("NEW_MESSAGE", this.onMessage.bind(this));
+    Logger.info("NEW WEBSOCKET CONNECTION ID :", connection.id);
+
+    connection.on("disconnecting", this.userDisconnected.bind(this));
+
+    for (let [event] of this.subscribers) {
+      connection.on(event, ((eventData) => this.onMessage(event, eventData)).bind(this));
+    }
   }
 
-  async userDisconnected(): Promise<void> {
-    throw new Error("Method not implemented.");
+  async userDisconnected(eventData: any): Promise<void> {
+    console.log(eventData);
   }
 
-  async onMessage(message: any): Promise<void> {
-    const chatMessage = ChatMessage.create(message.userId, message.text);
-    this.socket.emit("NEW_MESSAGE", chatMessage);
+  async onMessage(eventName: string, message: any): Promise<void> {
+    const handler = this.subscribers.get(eventName);
+    const eventHandlerResult = await handler(message);
+
+    if (eventHandlerResult.emitAll) {
+      this.socket.emit(eventHandlerResult.emitEventName, eventHandlerResult.data);
+    }
   }
 
   async addConnectionUser(socket: Socket, userId: string) {
