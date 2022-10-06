d668fa1e8b32c4a1a9211ddce0ead816cd65bf6f
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-06 17:17:04 -0400

    Changed the NeMessageuseCase to receive ChatRepository

--- src/application/chat/useCases/NewMessage.ts
+++ src/application/chat/useCases/NewMessage.ts
@@ -1,9 +1,27 @@
+import { UsersRepository } from "@application/accounts/repositories/IUsersRepository";
 import { ChatMessage } from "@domain/entities/ChatMessage";
 import { NewMessageInDto, NewMessageOutDto } from "../dtos/NewMessageTDO";
+import { ChatRepository } from "../repositories/ChatRepository";
+import { AppError } from "@shared/errors/AppError";
 
 export class NewMessageUseCase {
+  constructor(private readonly chatRepository: ChatRepository, private readonly usersRepositoty: UsersRepository) {}
+
   async execute(data: NewMessageInDto): Promise<NewMessageOutDto> {
+    const existsRoom = await this.chatRepository.existsRoomById(data.roomId);
+
+    if (!existsRoom) {
+      throw new AppError("Room does not exists!");
+    }
+
+    const existsUser = await this.usersRepositoty.findById(data.userId);
+
+    if (!existsUser) {
+      throw new AppError("user does not exists!");
+    }
+
     const message = ChatMessage.create(data.userId, data.text);
-    return { ...message, created: message.created.toLocaleString("pt-br") };
+    this.chatRepository.saveMessage(data.roomId, message);
+    return { ...message, roomId: data.roomId, created: message.created.toLocaleString("pt-br") };
   }
 }
