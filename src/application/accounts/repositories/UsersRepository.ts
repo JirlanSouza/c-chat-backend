220ad24d43a4da97574565ad9319e6036776d2e1
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-06 17:14:42 -0400

    Changed the IUserRepository interface name to UsersRepository

--- src/application/accounts/repositories/UsersRepository.ts
+++ src/application/accounts/repositories/UsersRepository.ts
@@ -1,6 +1,6 @@
 import { User } from "@domain/entities/User";
 
-export interface IUsersRepository {
+export interface UsersRepository {
   save(user: User): Promise<void>;
   findByEmail(email: string): Promise<User | undefined>;
   findById(id: string): Promise<User | undefined>;
