248a0ddaee95f7f134bb08075d6a670b59896273
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-05 22:29:44 -0400

    Changed the newMessageEventHandler constructor and handler return

--- src/infra/webSocket/events/handlers/newMessageEventHandler.ts
+++ src/infra/webSocket/events/handlers/newMessageEventHandler.ts
@@ -1,17 +1,24 @@
 import { NewMessageUseCase } from "@application/chat/useCases/NewMessage";
+import { EventEmitterGatway } from "../EventEmitterGatway";
 import { EventHandlerOut } from "./types";
 
 type NewMessageEventData = { userId: string; text: string };
 
 export class NewMessageEventHandler {
-  constructor(private newMessageUsecase: NewMessageUseCase) {}
+  private listenerEventName = "NEW_MESSAGE";
+  private emitEventName = "NEW_MESSAGE";
 
-  async handler(eventData: NewMessageEventData): Promise<EventHandlerOut> {
+  constructor(private eventEmitterGatway: EventEmitterGatway, private newMessageUsecase: NewMessageUseCase) {
+    this.eventEmitterGatway.subscribeEvent(this.listenerEventName, this.handler.bind(this));
+  }
+
+  private async handler(eventData: NewMessageEventData): Promise<EventHandlerOut> {
     const { userId, text } = eventData;
     const newMessageresult = await this.newMessageUsecase.execute({ userId, text });
 
     return {
       emitAll: true,
+      emitEventName: this.emitEventName,
       data: newMessageresult,
     };
   }
