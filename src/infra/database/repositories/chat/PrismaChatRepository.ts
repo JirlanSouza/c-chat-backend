472ec0ea03f5d0bf7487c1d7a9b17cde20237f0b
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-07 19:11:24 -0400

    Fix promise awit in the create room message

--- src/infra/database/repositories/chat/PrismaChatRepository.ts
+++ src/infra/database/repositories/chat/PrismaChatRepository.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { ChatRepository } from "@application/chat/repositories/ChatRepository";
 import { ChatMessage } from "@domain/entities/ChatMessage";
 import { AppError } from "@shared/errors/AppError";
+import { RoomDto, MessageDto } from "@application/chat/dtos/GetLastRoomMessagesDTO";
 
 export class PrismaChatRepository implements ChatRepository {
   private prisma = new PrismaClient();
@@ -13,7 +14,7 @@ export class PrismaChatRepository implements ChatRepository {
   }
 
   async saveMessage(roomId: string, message: ChatMessage): Promise<void> {
-    const savedMessage = this.prisma.roomMessage.create({
+    const savedMessage = await this.prisma.roomMessage.create({
       data: {
         id: message.id,
         roomId,
@@ -26,5 +27,23 @@ export class PrismaChatRepository implements ChatRepository {
     if (!savedMessage) {
       throw new AppError("Error when save message", 500);
     }
+  }
+
+  async getRoomByTitle(title: string): Promise<RoomDto> {
+    const room = await this.prisma.room.findUnique({ where: { title } });
+    return room;
+  }
+
+  async getMessages(roomId: string, dateEnd: number, maxMessage: number): Promise<MessageDto[]> {
+    const messages = await this.prisma.roomMessage.findMany({
+      where: { roomId },
+    });
+
+    return messages.map((message) => {
+      return {
+        ...message,
+        created: message.created.toLocaleString("pt-br", { timeZone: "America/Sao_Paulo" }),
+      };
+    });
   }
 }
