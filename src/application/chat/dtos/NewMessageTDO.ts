efb8bdf95aaefaf2c67d556a0b9307346a9ea1b7
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-07 21:13:52 -0400

    Change the userid to user in the NewMessageTDO

--- src/application/chat/dtos/NewMessageTDO.ts
+++ src/application/chat/dtos/NewMessageTDO.ts
@@ -7,7 +7,13 @@ export class NewMessageInDto {
 export interface NewMessageOutDto {
   id: string;
   roomId: string;
-  userId: string;
+  user: User;
   text: string;
   created: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  avatarUrl: string;
 }
