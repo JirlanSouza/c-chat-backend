21c47671d5e0e1a1db9f9d86170f3fa981f0f27f
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-08 22:27:37 -0400

    Fix the message text return

--- src/infra/http/midllewares/errorVerification.ts
+++ src/infra/http/midllewares/errorVerification.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 
 import { AppError } from "@shared/errors/AppError";
+import { Logger } from "@shared/logger";
 
-export const errorVerification = (err: Error, request: Request, response: Response, next: NextFunction): Response => {
+export const errorVerification = (
+  err: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Response => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({ message: err.message });
   }
 
-  return response.status(500).json({ status: "error", message: `Internal server error ${err.message}` });
+  Logger.error(err.message);
+  return response.status(500).json({ status: "error", message: `Internal server error!` });
 };
