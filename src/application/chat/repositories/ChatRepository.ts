3436a598dc65b72fe3fff88509d19fd4b70c41b8
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-10 23:07:16 -0400

    Added the saveRoom in the ChatRepository

--- src/application/chat/repositories/ChatRepository.ts
+++ src/application/chat/repositories/ChatRepository.ts
@@ -1,10 +1,12 @@
 import { ChatMessage } from "@domain/entities/ChatMessage";
+import { Room } from "@domain/entities/Room";
 import { MessageDto, RoomDto } from "../dtos/GetLastRoomMessagesDTO";
 import { RoomDto as RoomWithLastMessageDatetimeDto } from "../dtos/GetRoomListDTO";
 
 export interface ChatRepository {
   existsRoomById: (id: string) => Promise<boolean>;
   saveMessage: (roomId: string, message: ChatMessage) => Promise<void>;
+  saveRoom: (room: Room) => Promise<void>;
   getRoomByName: (name: string) => Promise<RoomDto>;
   getMessages: (roomId: string, dateEnd: number, maxMessage: number) => Promise<MessageDto[]>;
   findRoomByUserId: (userId: string) => Promise<RoomWithLastMessageDatetimeDto[]>;
