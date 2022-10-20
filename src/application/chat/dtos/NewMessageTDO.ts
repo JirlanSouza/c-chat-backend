8abe35c498e2d61ebadf1fbe3d7d5df84404fa73
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-19 22:24:11 -0400

    Added the create files in new message use case

--- src/application/chat/dtos/NewMessageTDO.ts
+++ src/application/chat/dtos/NewMessageTDO.ts
@@ -2,6 +2,13 @@ export class NewMessageInDto {
   roomId: string;
   userId: string;
   text: string;
+  files?: MessageFileData[];
+}
+
+export interface MessageFileData {
+  fileName: string;
+  fileType: string;
+  fileSize: number;
 }
 
 export interface NewMessageOutDto {
@@ -9,7 +16,16 @@ export interface NewMessageOutDto {
   roomId: string;
   user: User;
   text: string;
+  files?: MessageFileOutData[];
   created: string;
+}
+
+export interface MessageFileOutData {
+  id: string;
+  name: string;
+  type: string;
+  available: boolean;
+  url: string;
 }
 
 export interface User {
