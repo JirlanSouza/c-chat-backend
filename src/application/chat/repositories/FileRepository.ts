ee895942229ae9472ffdc8a94f19ab410ed6776d
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-20 21:24:23 -0400

    Added the findById method in FileRepository interface

--- src/application/chat/repositories/FileRepository.ts
+++ src/application/chat/repositories/FileRepository.ts
@@ -2,4 +2,5 @@ import { File } from "@domain/entities/File";
 
 export interface FileRepository {
   saveMany: (messageId: string, files: File[]) => Promise<void>;
+  findById: (id: string) => Promise<File>;
 }
