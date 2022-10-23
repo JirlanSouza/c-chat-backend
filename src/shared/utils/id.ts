34ca2efab895d9fe0711d25f9b825b9cdf0f0e67
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-23 16:47:43 -0400

    Creted the isValidId function id validation

--- src/shared/utils/id.ts
+++ src/shared/utils/id.ts
@@ -3,3 +3,7 @@ import { ObjectId } from "bson";
 export function generateId(): string {
   return new ObjectId().toString();
 }
+
+export function isValidId(id: string): boolean {
+  return ObjectId.isValid(id);
+}
