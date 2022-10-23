d7333193d0447bee25b1b4682780e99ce8dde26c
Jirlan Souza <jirlansouza08@gmail.com>
2022-10-22 20:54:04 -0400

    Added the update method in the MessageFileRepository interface

--- src/application/chat/repositories/MessageFileRepository.ts
+++ src/application/chat/repositories/MessageFileRepository.ts
@@ -3,4 +3,5 @@ import { File } from "@domain/entities/File";
 export interface MessageFileRepository {
   saveMany: (messageId: string, files: File[]) => Promise<void>;
   findById: (id: string) => Promise<File>;
+  update: (file: File) => Promise<void>;
 }
